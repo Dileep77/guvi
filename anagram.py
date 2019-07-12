fdd05c02a29ee8e7a9df660b8fb6729dae243698
Dileep77 <35861015+Dileep77@users.noreply.github.com>
2019-07-12 11:44:59 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,4 +1,6 @@
 dhoni = ['d','h','i','n','o']
 s = input()
-if sorted(s) == dhoni:
-	print('yes')# your code goes here
+if sorted(s) == dhoni and len(s)==5:
+	print('yes')
+else:
+	print("no")# your code goes here
