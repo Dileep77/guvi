d48515f5096826357abc2f29659889f8969ac414
Dileep77 <35861015+Dileep77@users.noreply.github.com>
2019-07-12 23:44:24 +0530

    Create uvminarray.py

---
+++ uvminarray.py
@@ -0,0 +1,11 @@
+a,b=input().split()
+h=list(map(int,input().split()))
+for p in range(int(b)):
+	u,v=input().split()
+	sum=0
+	for z in range(int(u)-1,int(v)):
+		min=h[int(u)-1]
+		if h[z]<min:
+			min=h[z]	
+	print(min)
+	continue# your code goes here
