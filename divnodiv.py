38f70c5a8faa11524e6d400ff011a6887c55d553
Dileep77 <35861015+Dileep77@users.noreply.github.com>
2019-07-17 00:09:36 +0530

    Update divnodiv.py

--- divnodiv.py
+++ divnodiv.py
@@ -1,9 +1,14 @@
 a,b=list(map(int,input().split()))
 sum=0
 count=0
-while(True):
-  sum=sum+b
-  count+=1
-  if sum==a:
-    print(count)
-    break
+if a==0:
+	print("0")
+	
+else:
+	if a>=b:
+		while(True):
+			sum=sum+b
+			count+=1
+			if sum==a:
+				print(count)
+				break
