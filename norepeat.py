73aac6812ac27559886a4ae0efabb73e9a7dcfe4
Dileep77 <35861015+Dileep77@users.noreply.github.com>
2019-08-02 15:58:34 +0530

    Update norepeat.py

--- norepeat.py
+++ norepeat.py
@@ -1,6 +1,9 @@
 a=int(input())
 b=list(map(int,input().split()))
+k=[]
 for i in range(len(b)):
-	if b[i] not in b[i+1:]:
-		print(b[i])
-		break
+	if b[i] in b[i+1:]:
+		k.append(b[i])
+for j in range(len(b)):
+	if b[j] not in k:
+		print(b[j])
